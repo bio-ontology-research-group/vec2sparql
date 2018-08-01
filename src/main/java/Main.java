2aa9fd21470cded0adcef88afa8335e3e3cbe8d1
Maxat Kulmanov <coolmaksat@gmail.com>
2018-08-01 21:50:25 +0300

    sparql endpoint for knowledge graph

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,16 @@
 import org.apache.jena.fuseki.embedded.FusekiServer;
 import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.query.Dataset;
-
+import org.apache.jena.atlas.logging.LogCtl;
+import org.apache.jena.riot.RDFDataMgr;
 
 public class Main {
 
     public static void main(String[] args) {
-	Dataset ds = DatasetFactory.createTxnMem();
+	LogCtl.setJavaLogging();
+	Dataset ds = RDFDataMgr.loadDataset("data/bio-knowledge-graph.n3");
 	FusekiServer fs = FusekiServer.create()
-	    .add("/ds", ds)
+	    .add("/ds", ds, true)
 	    .build();
 	
 	fs.start();
