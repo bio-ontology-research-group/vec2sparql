4cf6960f191b0744cc955e4cb201b13b05e99eba
Maxat Kulmanov <coolmaksat@gmail.com>
2018-08-26 02:10:15 +0300

    frontend example query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+	super(props);
+	
+	this.state = {
+	    query: '',
+	};
+
+	this.handleExampleClick = this.handleExampleClick.bind(this);
+    }
+    
+    handleExampleClick(event) {
+	var query = "PREFIX b2v: <http://bio2vec.net/function#> \nSELECT ?s ?o {?s ?p ?o FILTER (b2v:similarity(?s, ?o) = 1)} \nLIMIT 5";
+	this.setState({ query: query });
+    }
+    
+    render() {
+	return (
+		<div className="App">
+		<header className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+		<h1 className="App-title">Vec2SPARQL</h1>
+		</header>
+		<main className="container">
+		<p><a href="/#" onClick={this.handleExampleClick} >Example 1</a></p>
+		<div className="text-center">
+		<form className="form form-sparql" action="/ds/query" method="get">
+		<div className="form-group">
+		<label for="query">Enter SPARQL Query</label>
+		<textarea className="form-control" name="query" id="query" rows="5" value={this.state.query}/>
+		</div>
+		<button className="btn btn-primary" type="submit">Submit</button>
+		</form>
+		</div>
+		</main>
+		<footer className="footer">
+		</footer>
+		</div>
+	);
+    }
 }
 
 export default App;
