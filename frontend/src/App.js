646361c0a1c1f710e8b1405f3a4502d977d0b546
Maxat Kulmanov <coolmaksat@gmail.com>
2018-08-26 03:12:12 +0300

    logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo_small.png';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
